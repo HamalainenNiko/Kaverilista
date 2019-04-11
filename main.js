465dbf0e721255e927913bc2eb9c3e887b07dae5
Niko Hämäläinen <niko.hamalainen@itvelhot.local>
2019-04-11 11:47:25 +0300

    nysse_toimii

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
+function myFunction() {
 
+    var i = 0
+
+
+    do {
+        var person = prompt("Lisää nimi");
+        i++;
+    document.write(person + "<br/>");   
+ }
+
+    while(i < 10) {
+    
+    }
+
+}
